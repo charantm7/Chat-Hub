fb8a3fff46081967b41f863a3d656553f17849f1
charantm <charantm8787@gmail.com>
2025-07-26 23:04:03 +0530

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,8 @@ import ChatRoom from "./chatroom";
 
 function App() {
   // ✅ Replace with your actual chat_id and JWT token
-  const chatId = "6c0b4971-5963-48dc-91c8-8d824ee06aef";
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNoYXJhbm50bS5kZXZAZ21haWwuY29tIiwiZXhwIjoxNzUzNTY1NTUyfQ.Rdt70S9swdF7lKsvW6xngcJQSrSaCgEP3n1JUEBoyh8";
+  const chatId = "";
+  const token = "";
 
   return (
     <div>
