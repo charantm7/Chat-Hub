7eb567c4911500ed1ec29a893672158724e9cead
charantm <charantm8787@gmail.com>
2025-07-27 14:54:14 +0530

    created react project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import ChatRoom from "./chatroom";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
-function App() {
-  // ✅ Replace with your actual chat_id and JWT token
-  const chatId = "";
-  const token = "";
-
+const App = () => {
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>🚀 FastAPI WebSocket Chat</h1>
-      <ChatRoom chat_id={chatId} token={token} />
+    <div className="bg-[#0d1117f1] flex flex-col p-[.7rem] gap-[.7rem]">
+      <Navbar />
+      <Main />
     </div>
   );
-}
+};
 
 export default App;
